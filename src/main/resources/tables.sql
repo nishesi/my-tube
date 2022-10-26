5f52175c82d803d9ede01cb0bc119452f00f6b09
Nurislam Zaripov <znurislama56@gmail.com>
2022-10-26 19:34:29 +0300

    update tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,39 +1,48 @@
 create table users
 (
     user_id    bigserial primary key,
-    password   char(50) not null,
-    first_name varchar(15),
-    last_name  varchar(15),
-    birthdate  date,
-    country    varchar(20)
+    password   char(50)    not null,
+    first_name varchar(15) not null,
+    last_name  varchar(15) not null,
+    birthdate  date        not null,
+    country    varchar(20) not null
+);
+
+create table user_subscriptions
+(
+    user_id    bigint references users (user_id)                   not null,
+    channel_id bigint references channel_covers (channel_cover_id) not null
 );
 
 create table video_covers
 (
     video_cover_id   bigserial primary key,
-    video_name       varchar(70) not null,
-    icon             bytea,
-    added_date       timestamp with time zone,
-    channel_cover_id bigint references channel_covers (channel_cover_id),
-    video_id         bigint references videos (video_id),
-    duration         time        not null
+    video_name       varchar(70)                                         not null,
+--  video_icon - name of the file that store image in storage
+    video_icon       uuid                                                not null,
+    added_date       timestamp with time zone                            not null,
+    channel_cover_id bigint references channel_covers (channel_cover_id) not null,
+    video_id         uuid references videos (video_id),
+    duration         time                                                not null
 );
 
 create table videos
 (
-    video_id bigint primary key,
-    source   varchar not null,
-    info     varchar(1000),
-    likes    bigint,
-    dislikes bigint,
-    views    bigint
+--  video_id is the name of the file
+--  that store video content in the storage
+    video_id uuid primary key,
+    info     varchar(1000) default '',
+    likes    bigint        default 0,
+    dislikes bigint        default 0,
+    views    bigint        default 0
 );
 
 create table channel_covers
 (
     channel_cover_id bigserial primary key,
     channel_name     varchar(20) not null,
-    channel_icon     bytea,
+--  channel_icon - name of the file that store image in storage
+    channel_icon     uuid        not null,
     channel_id       bigint references channels (channel_id)
 );
 
@@ -41,7 +50,13 @@ create table channels
 (
     channel_id   bigserial primary key,
     owner_id     bigint references users (user_id),
-    subs_count   bigint,
-    channel_info varchar(1000),
+    subs_count   bigint        default 0,
+    channel_info varchar(1000) default '',
     videos_json  json not null
+);
+
+create table channel_videos
+(
+    channel_id     bigint references channels (channel_id)         not null,
+    video_cover_id bigint references video_covers (video_cover_id) not null
 )
