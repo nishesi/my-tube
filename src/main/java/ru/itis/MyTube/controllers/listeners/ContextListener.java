7105e008013139d6354636860bd5833721b4b987
Nurislam Zaripov <znurislama56@gmail.com>
2022-10-31 09:36:49 +0300

    implement destroy method

--- src/main/java/ru/itis/MyTube/controllers/listeners/ContextListener.java
+++ src/main/java/ru/itis/MyTube/controllers/listeners/ContextListener.java
@@ -2,28 +2,35 @@ package ru.itis.MyTube.controllers.listeners;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import ru.itis.MyTube.auxilary.Attributes;
+import ru.itis.MyTube.auxiliary.Attributes;
 import ru.itis.MyTube.model.dao.implementations.UserRepJdbcImpl;
 import ru.itis.MyTube.model.dao.interfaces.UserRepository;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ContextListener implements ServletContextListener {
+
+    private HikariDataSource dataSource;
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
-        DataSource dataSource = initDataSource();
+        dataSource = initDataSource();
+
 
         UserRepository userRepository = new UserRepJdbcImpl(dataSource);
         context.setAttribute(Attributes.USER_REP.toString(), userRepository);
     }
 
-    private DataSource initDataSource() {
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        dataSource.close();
+    }
+
+    private HikariDataSource initDataSource() {
         Properties properties = new Properties();
 
         try {
