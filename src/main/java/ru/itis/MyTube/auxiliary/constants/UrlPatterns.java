5fce41079ee08d337c32edf379b20a6558f207dd
Nurislam Zaripov <znurislama56@gmail.com>
2022-11-07 21:47:22 +0300

    add url patterns

---
+++ src/main/java/ru/itis/MyTube/auxiliary/constants/UrlPatterns.java
@@ -0,0 +1,12 @@
+package ru.itis.MyTube.auxiliary.constants;
+
+public class UrlPatterns {
+    public static final String SEARCH_PAGE = "/search";
+    public static final String WATCH_PAGE = "/watch";
+    public static final String VIDEO_UPLOAD_PAGE = "/add/video";
+    public static final String REGISTRATION_PAGE = "/register";
+    public static final String AUTHENTICATION_PAGE = "/authenticate";
+    public static final String RESOURCE = "/resource";
+    public static final String CHANNEL_PAGE = "/channel";
+    public static final String USER_PAGE = "/user";
+}
