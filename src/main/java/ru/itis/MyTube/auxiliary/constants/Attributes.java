a6f4796ff52a09a00637a645ef9178df8f568f14
Nurislam Zaripov <znurislama56@gmail.com>
2022-11-08 17:28:27 +0300

    add constant classes

---
+++ src/main/java/ru/itis/MyTube/auxiliary/constants/Attributes.java
@@ -0,0 +1,9 @@
+package ru.itis.MyTube.auxiliary.constants;
+
+public class Attributes {
+    public static final String VIDEO_COVER_LIST = "videoCoverList";
+    public static final String USER = "user";
+    public static final String APP_LOGO_URL = "logoUrl";
+    public static final String APP_NAME = "appName";
+    public static final String COMMON_CSS_URL = "commonCssUrl";
+}
