20aeae70e498c062e101f17a818638311d93c04e
Nurislam Zaripov <znurislama56@gmail.com>
2023-04-22 21:41:33 +0300

    files

---
+++ src/main/java/ru/itis/MyTube/MyTubeApplication.java
@@ -0,0 +1,12 @@
+package ru.itis.MyTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+@SpringBootApplication
+public class MyTubeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyTubeApplication.class, args);
+    }
+}
