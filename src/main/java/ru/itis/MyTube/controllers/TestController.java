376187aa49bf38c277b03d874c68c986d4c0e9d0
Nurislam Zaripov <znurislama56@gmail.com>
2023-04-23 15:45:06 +0300

    configure pebble templates

---
+++ src/main/java/ru/itis/MyTube/controllers/TestController.java
@@ -0,0 +1,14 @@
+package ru.itis.MyTube.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String get() {
+        return "test";
+    }
+}
