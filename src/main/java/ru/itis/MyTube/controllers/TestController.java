5a4d6650e19c32dacfd3c17d2493820cf319bcf2
Nurislam Zaripov <znurislama56@gmail.com>
2023-04-28 17:14:40 +0300

    refactoring and forgot template

--- src/main/java/ru/itis/MyTube/controllers/TestController.java
+++ src/main/java/ru/itis/MyTube/controllers/TestController.java
@@ -2,10 +2,11 @@ package ru.itis.MyTube.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
--- src/main/java/ru/itis/MyTube/dto/forms/UpdateUserForm.java
+++ src/main/java/ru/itis/MyTube/dto/forms/UpdateUserForm.java
@@ -1,7 +1,5 @@
 package ru.itis.MyTube.dto.forms;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
